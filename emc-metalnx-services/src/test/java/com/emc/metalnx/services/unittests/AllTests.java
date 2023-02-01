2ead2001db1afe7f64c686a4ff5f614c679a28f4
Justin James <jjames@renci.org>
2023-02-01 15:10:03 -0500

    [DICE-UNC/jargon415] Added metadata test suite into AllTests.java

--- emc-metalnx-services/src/test/java/com/emc/metalnx/services/unittests/AllTests.java
+++ emc-metalnx-services/src/test/java/com/emc/metalnx/services/unittests/AllTests.java
@@ -1,12 +1,13 @@
 package com.emc.metalnx.services.unittests;
 
 import com.emc.metalnx.services.irods.CollectionServiceImplTest;
+import com.emc.metalnx.services.irods.TestAddMetadataToObjs;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ CollectionServiceImplTest.class })
+@Suite.SuiteClasses({ CollectionServiceImplTest.class, TestAddMetadataToObjs.java })
 public class AllTests {
 
 }
