66d0957eb63160dcd02c6c9de0d6d3c85f80b13c
Justin James <jjames@renci.org>
2023-02-01 15:14:26 -0500

    tm

--- emc-metalnx-services/src/test/java/com/emc/metalnx/services/unittests/AllTests.java
+++ emc-metalnx-services/src/test/java/com/emc/metalnx/services/unittests/AllTests.java
@@ -1,13 +1,13 @@
 package com.emc.metalnx.services.unittests;
 
 import com.emc.metalnx.services.irods.CollectionServiceImplTest;
-import com.emc.metalnx.services.irods.TestAddMetadataToObjs;
+import com.emc.metalnx.services.tests.metadata.TestAddMetadataToObjs;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ CollectionServiceImplTest.class, TestAddMetadataToObjs.java })
+@Suite.SuiteClasses({ CollectionServiceImplTest.class, TestAddMetadataToObjs.class })
 public class AllTests {
 
 }
